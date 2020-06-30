034cd0cfbd2b76cca171e090f9142fcf6e45dcc6
Edward White <edw@rdjam.es>
2020-06-30 10:35:58 -0400

    Add files via upload

---
+++ Pokedex/Pokedex/PokemonListViewController.swift
@@ -0,0 +1,105 @@
+import UIKit
+
+class PokemonListViewController: UITableViewController, UISearchBarDelegate {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
+    var searchActive : Bool = false
+    var pokemon: [PokemonListResult] = []
+    var filtered: [PokemonListResult] = []
+    
+    func capitalize(text: String) -> String {
+        return text.prefix(1).uppercased() + text.dropFirst()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.delegate = self
+        
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let entries = try JSONDecoder().decode(PokemonListResults.self, from: data)
+                self.pokemon = entries.results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filtered = pokemon.filter({ ( pokemon: PokemonListResult) -> Bool in
+            let tmp: NSString = pokemon.name as NSString
+            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (searchActive){
+            return filtered.count
+        }
+        return pokemon.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        if (searchActive){
+            cell.textLabel?.text = capitalize(text: filtered[indexPath.row].name)
+        } else {
+            cell.textLabel?.text = capitalize(text: pokemon[indexPath.row].name)
+        }
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowPokemonSegue",
+                let destination = segue.destination as? PokemonViewController,
+                let index = tableView.indexPathForSelectedRow?.row {
+            destination.url = pokemon[index].url
+        }
+    }
+}
